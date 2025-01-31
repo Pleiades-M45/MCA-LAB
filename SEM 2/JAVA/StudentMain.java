bebdb2149c7b51936774963db0e5166aa71e848e
Thasneem Shahul <139751320+Pleiades-M45@users.noreply.github.com>
2025-01-31 11:22:20 +0530

    Update StudentMain.java

--- SEM 2/JAVA/StudentMain.java
+++ SEM 2/JAVA/StudentMain.java
@@ -1,38 +1,42 @@
 import java.util.Scanner;
-class Student{
+
+class Student {
     int rollno, marks[];
     String name;
     static int count = 0;
-    public Student(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("RollNo : ");
+
+    public Student(Scanner sc) {
+        System.out.print("RollNo: ");
         rollno = sc.nextInt();
-        System.out.print("Name : ");
+        System.out.print("Name: ");
         name = sc.next();
-        System.out.print("\tMarks (3 subjects) : \n");
+        System.out.println("\tMarks (3 subjects): ");
         marks = new int[3];
-        for(int i=0;i<3;i++){
+        for (int i = 0; i < 3; i++) {
             marks[i] = sc.nextInt();
         }
         count++;
     }
-    public int total(){
+
+    public int total() {
         int sum = 0;
-        for(int i = 0; i < 3; i++){
-            sum+=marks[i];
+        for (int i = 0; i < 3; i++) {
+            sum += marks[i];
         }
         return sum;
     }
-    public void display(){
-        System.out.println("RollNo : "+ rollno + "\nName : "+ name + "\nMarks : ["+ marks[0] + ", " + marks[1] + ", " + marks[2] + "]");
+
+    public void display() {
+        System.out.println("RollNo: " + rollno + "\nName: " + name + "\nMarks: [" + marks[0] + ", " + marks[1] + ", " + marks[2] + "]");
     }
 }
-public class StudentMain{
-    public static void sortStudent(Student ob[]){
+
+public class StudentMain {
+    public static void sortStudent(Student ob[]) {
         int n = Student.count;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(ob[i].total() < ob[j].total()){
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (ob[i].total() < ob[j].total()) {
                     Student temp = ob[i];
                     ob[i] = ob[j];
                     ob[j] = temp;
@@ -40,23 +44,25 @@ public class StudentMain{
             }
         }
     }
+
     public static void main(String[] args) {
-        Student ob[] = new Student[10];
         Scanner scanner = new Scanner(System.in);
-        System.out.print("No. of Students : ");
+        System.out.print("No. of Students: ");
         int n = scanner.nextInt();
+
+        Student[] ob = new Student[n];
         for (int i = 0; i < n; i++) {
-            ob[i] = new Student();
+            ob[i] = new Student(scanner);
         }
-        // for (int i = 0; i < 3; i++) {
-        //     ob[i].display();
-        // }
+
         sortStudent(ob);
+
         System.out.println("\t---Ranklist---\n");
         for (int i = 0; i < n; i++) {
             ob[i].display();
-            System.out.println("Total Marks : "+ ob[i].total() + "\n");
+            System.out.println("Total Marks: " + ob[i].total() + "\n");
         }
+
         scanner.close();
     }
 }
